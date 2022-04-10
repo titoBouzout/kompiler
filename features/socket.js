6c1f308f29f912757b489a5b980ce2b06404d70f
Tito <tito.bouzout@gmail.com>
2022-04-10 13:11:36 -0300

    Colored output for messages coming from the socket server

--- features/socket.js
+++ features/socket.js
@@ -1,38 +1,16 @@
 promise(async function socket() {
 	if (options.socket && options.socket.input && (await exists(project + options.socket.input))) {
 		let socketServer
-		let printOutPut = true
+
+		subtitle('Starting Socket Server')
+		console.log()
 
 		function socketStart() {
 			const { spawn } = require('child_process')
 			let command = options.socket.es6
 				? ['-r', project + 'node_modules/esm/index.js', options.socket.input]
 				: [options.socket.input]
-			const socketServer = spawn('node', command)
-
-			socketServer.on('error', e => {
-				// This will be called with err being an AbortError if the controller aborts
-				if (printOutPut) {
-				}
-				console.error(e)
-			})
-			socketServer.on('data', e => {
-				// This will be called with err being an AbortError if the controller aborts
-				if (printOutPut) {
-					console.error(e)
-				}
-			})
-			socketServer.stdout.on('data', data => {
-				if (printOutPut) {
-					console.log('Socket Server:', data.toString())
-				}
-			})
-			socketServer.stderr.on('data', data => {
-				if (printOutPut) {
-					console.error('Socket Server:', data.toString())
-				}
-			})
-
+			const socketServer = spawn('node', command, { stdio: 'inherit' })
 			return socketServer
 		}
 
@@ -46,13 +24,12 @@ promise(async function socket() {
 			let hash = await hash_file(file)
 			if (hash != hashes[file]) {
 				if (file) {
-					log('Restarting Socket Server')
+					subtitle('Restarting Socket Server - ' + file.replace(project, ''))
+					console.log()
 				}
 				hashes[file] = hash
-				printOutPut = false
-				socketServer && socketServer.kill() // Stops the child process
-				printOutPut = true
 
+				socketServer && socketServer.kill() // Stops the child process
 				socketServer = socketStart()
 			}
 		}
