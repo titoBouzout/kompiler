1a5ca0597073de16faf4bcd2f80d60f9ca5f12cc
Tito Bouzout <tito.bouzout@gmail.com>
2023-03-26 18:34:35 -0300

    Only check npm outdate once a day

--- features/npm.js
+++ features/npm.js
@@ -1,23 +1,27 @@
 promise(async function npm() {
-	const { spawn } = require('child_process')
+	if ((await db('npm updates')) !== today()) {
+		db('npm updates', today())
 
-	function outdated(folder) {
-		spawn(is_windows() ? 'npm.cmd' : 'npm', ['outdated'], {
-			stdio: 'inherit',
-			cwd: folder,
-		})
-	}
+		const { spawn } = require('child_process')
 
-	// watch all folders with packages json to run npm outdated
-	let packagesJson = await find(project, 'package.json')
+		function outdated(folder) {
+			spawn(is_windows() ? 'npm.cmd' : 'npm', ['outdated'], {
+				stdio: 'inherit',
+				cwd: folder,
+			})
+		}
 
-	for (let package of packagesJson) {
-		let folder = dirname(package)
-		setTimeout(function () {
-			outdated(folder)
-		}, 5000)
-		watch(null, package, function () {
-			outdated(folder)
-		})
+		// watch all folders with packages json to run npm outdated
+		let packagesJson = await find(project, 'package.json')
+
+		for (let package of packagesJson) {
+			let folder = dirname(package)
+			setTimeout(function () {
+				outdated(folder)
+			}, 5000)
+			watch(null, package, function () {
+				outdated(folder)
+			})
+		}
 	}
 })
