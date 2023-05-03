ad8bbf76fa872c1483852892411a70ba3aaf8956
Tito Bouzout <tito.bouzout@gmail.com>
2023-05-03 03:11:51 -0300

    Do not "git init"

--- features/git-status.js
+++ features/git-status.js
@@ -2,10 +2,11 @@ no_git_status = false
 
 promise(function git() {
 	run(
-		'git init && git config merge.ours.driver true && git config color.ui auto && git config diff.mnemonicPrefix true && git config diff.renames true',
+		'git config merge.ours.driver true && git config color.ui auto && git config diff.mnemonicPrefix true && git config diff.renames true',
 		{
 			sync: true,
 			callback: noop,
+			nostderr: true,
 		},
 	)
 	let command = {
