8619829c18cf0b6bb7abab333ea25faca77eb86d
Tito Bouzout <tito.bouzout@gmail.com>
2023-11-04 02:44:34 -0300

    Improvements to file server

--- features/serve.js
+++ features/serve.js
@@ -6,133 +6,192 @@ open_in_browser = function () {
 }
 
 promise(async function serve() {
-	for (let build of options.builds) {
-		build.root = dirname(build.input[0])
-		build.port = seeded_random(1025, 65534, build.root)
-		build.page = 'http://localhost:' + build.port
+	if (options.builds)
+		for (let build of options.builds) {
+			// do not serve
+			if (build.root === false) continue
 
-		pages.push(build.page)
+			build.root = build.root ? normalize(build.root) : dirname(build.input[0])
+			build.port = seeded_random(1025, 65534, build.root)
+			const host = 'http://localhost:' + build.port
+			const page = build.page ? host + '/' + build.page : host
+			if (!build.page) build.page = 'index.html'
+			pages.push(page)
 
-		subtitle('Serving ' + build.root)
+			subtitle('Serving ' + build.root)
 
-		log(build.page)
+			log(page)
 
-		if (!build.express) {
-			let mime = require('mime-types')
-			require('http')
-				.createServer(async function (req, res) {
-					async function serve(file) {
-						res.setHeader('Content-Type', mime.lookup(file))
-						res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-						res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+			if (!build.express) {
+				let mime = require('mime-types')
 
-						res.writeHead(200)
-						res.end(Buffer.from(await fs.promises.readFile(file)))
+				function contentType(mime) {
+					switch (mime) {
+						case 'text/html':
+						case 'text/css':
+
+						case 'application/javascript':
+						case 'application/json':
+						case 'application/jsx':
+
+						case 'text/javascript':
+						case 'text/json':
+						case 'text/jsx':
+
+						case 'text/plain':
+							return '; charset=utf-8'
+						default:
+							return ''
 					}
+				}
 
-					let file =
-						build.root + '/' + decodeURIComponent(req.url).replace(/^\//, '').replace(/\?.*/, '')
-
-					if (is_directory(file) && (await exists(file + 'index.html'))) {
-						file += 'index.html'
+				function _decodeURIComponent(a) {
+					try {
+						return decodeURIComponent(a)
+					} catch (e) {
+						console.log(a)
+						return a
 					}
+				}
+				require('http')
+					.createServer(async function (req, res) {
+						async function serve(file) {
+							let mimeType = mime.lookup(file)
+							res.setHeader('Content-Type', mimeType + contentType(mimeType))
+							// res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+							// res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
 
-					if ((await exists(file)) && !is_directory(file)) {
-						serve(file)
-					} else {
-						if (is_directory(file)) {
-							let files = await list(file)
-							let content = '<h1>' + file.replace(build.root + '/', '') + '</h1><hr/><ul>'
-							for (let f of files)
-								content +=
-									'<li><a href="/' +
-									f.replace(build.root + '/', '') +
-									'">' +
-									f.replace(build.root + '/', '') +
-									'</a>'
-
-							res.setHeader('Content-Type', 'text/html')
-							res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-							res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
 							res.writeHead(200)
-							res.end(content)
+							res.end(Buffer.from(await fs.promises.readFile(file)))
+						}
+
+						let file =
+							build.root +
+							'/' +
+							_decodeURIComponent(req.url.replace(/^\//, '').replace(/\?.*/, ''))
+
+						if (is_directory(file) && (await exists(file + build.page))) {
+							file += build.page
+						}
+
+						if ((await exists(file)) && !is_directory(file)) {
+							serve(file)
 						} else {
-							if (
-								(/\/[^\.]+$/.test(file) || /\/[^\.]+\.html$/.test(file)) &&
-								(await exists(build.root + '/index.html'))
-							) {
-								file = build.root + '/index.html'
-								serve(file)
+							if (is_directory(file)) {
+								let files = await list(file)
+								let content =
+									'<h1>' + file.replace(build.root + '/', '') + '</h1><hr/><ul>'
+								for (let f of files)
+									content +=
+										'<li><a href="/' +
+										f.replace(build.root + '/', '') +
+										'">' +
+										f.replace(build.root + '/', '') +
+										'</a>'
+
+								res.setHeader('Content-Type', 'text/html; charset=utf-8')
+								res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+								res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+								res.writeHead(200)
+								res.end(content)
 							} else {
-								if (build.map) {
-									for (const redirect of build.map) {
-										let target = file.replace(redirect[0], redirect[1])
-										if (await exists(target)) {
-											return serve(target)
+								if (
+									(/\/[^\.]+$/.test(file) || /\/[^\.]+\.html$/.test(file)) &&
+									(await exists(build.root + '/' + build.page))
+								) {
+									blue('map: ' + file.replace(build.root, '') + ' -> ' + '/' + build.page)
+									file = build.root + '/' + build.page
+									serve(file)
+								} else {
+									let map = build.map || options.map
+									if (map) {
+										for (const redirect of map) {
+											let target = file.replace(redirect[0], redirect[1])
+											if (await exists(target)) {
+												return serve(target)
+											}
 										}
 									}
+									error('404 Not Found: ' + file)
+									res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+									res.writeHead(404)
+									res.end('404 File not found: ' + file)
 								}
-								error('404 Not Found: ' + file)
-								res.setHeader('Content-Type', 'text/plain')
-								res.writeHead(404)
-								res.end()
 							}
 						}
-					}
+					})
+					.listen(build.port)
+			} else {
+				const express = require('express')
+				const app = express()
+				app.listen(build.port)
+				app.use(express.static(build.root))
+			}
+
+			// AUTO REFRESH
+			const WebSocket = require('ws')
+			build.wss = new WebSocket.Server({ port: build.port - 1 })
+
+			// watch index
+			if (await exists(build.root + '/' + build.page)) {
+				watch(null, build.root + '/' + build.page, function () {
+					line('Auto Refresh - ' + build.page + ' changed, refreshing')
+					build.wss.clients.forEach(function (client) {
+						client.send('reload')
+					})
 				})
-				.listen(build.port)
-		} else {
-			const express = require('express')
-			const app = express()
-			app.listen(build.port)
-			app.use(express.static(build.root))
-		}
+			} else {
+				warning(
+					build.root + '/' + build.page + ' - doesnt exists so wont be watched till then',
+				)
+			}
 
-		// AUTO REFRESH
-		const WebSocket = require('ws')
-		build.wss = new WebSocket.Server({ port: build.port - 1 })
+			// watch js and css
+			let reload_js = false
+			let reload_css = false
 
-		// watch index
-		watch(null, build.root + '/index.html', function () {
-			line('Auto Refresh - index.html changed, refreshing')
-			build.wss.clients.forEach(function (client) {
-				client.send('reload')
-			})
-		})
-
-		// watch js and css
-		let reload_js = false
-		let reload_css = false
-
-		watch(null, project + build.output, function () {
-			reload_js = true
-		})
-		watch(null, project + build.output.replace('.js', '.css'), function () {
-			reload_css = true
-		})
-
-		// wait for bundle to refresh
-		build.on_bundle_done = async function () {
-			if (reload_js) {
-				reload_js = false
-				line('Auto Refresh - js changed, refreshing')
-				build.wss.clients.forEach(function (client) {
-					client.send('reload')
+			// watch output
+			if (await exists(project + build.output)) {
+				watch(null, project + build.output, function () {
+					reload_js = true
 				})
-			} else if (reload_css) {
-				reload_css = false
-				line('Auto Refresh - css changed, hot reloading')
-				build.wss.clients.forEach(function (client) {
-					client.send('reload-css')
+			} else {
+				warning(project + build.output + ' - doesnt exists so wont be watched till then')
+			}
+
+			// watch output css
+			if (await exists(project + build.output.replace('.js', '.css'))) {
+				watch(null, project + build.output.replace('.js', '.css'), function () {
+					reload_css = true
 				})
+			} else {
+				warning(
+					project +
+						build.output.replace('.js', '.css') +
+						' - doesnt exists so wont be watched till then',
+				)
+			}
+
+			// wait for bundle to refresh
+			build.on_bundle_done = async function () {
+				if (reload_js) {
+					reload_js = false
+					line('Auto Refresh - js changed, refreshing')
+					build.wss.clients.forEach(function (client) {
+						client.send('reload')
+					})
+				} else if (reload_css) {
+					reload_css = false
+					line('Auto Refresh - css changed, hot reloading')
+					build.wss.clients.forEach(function (client) {
+						client.send('reload-css')
+					})
+				}
 			}
 		}
 
-		subtitle('Auto Refresh - watching: js, css, index.html')
-	}
-
-	if ((await db('opened in browser')) !== hour()) {
-		db('opened in browser', hour())
+	if ((await db('opened in browser')) !== today()) {
+		db('opened in browser', today())
 		open_in_browser()
 	}
 })
